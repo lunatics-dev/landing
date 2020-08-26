d046f9799f952309970fe24528b892869f37dc9a
Hector <hectordahv@gmail.com>
2020-08-26 11:31:30 -0500

    deploy

--- webpack.config.ts
+++ webpack.config.ts
@@ -19,7 +19,7 @@ const webpackConfig = (env: any): Configuration => ({
     },
   },
   output: {
-    path: path.join(__dirname, '/dist'),
+    path: path.join(__dirname, '/build'),
     filename: 'build.js',
     publicPath: '/',
   },
